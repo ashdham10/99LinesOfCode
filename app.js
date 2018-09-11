bdc9fc512c489199d594b100a8d41690ac9f41ef
ashdham10 <ashleydenham10@gmail.com>
2018-09-11 11:28:07 -0500

    Lab1Done

--- app.js
+++ app.js
@@ -5,14 +5,15 @@ for (var i = 0; i < friends.length; i++) {
 
     for (var j = 99; j >= 1; j--) {
       
-    if(j >= 2, j <= 99) {
+    if(j >= 2) {
         console.log([j] + ' lines of code in the file, ' + [j] +
-        ' lines of code; ' + friends[i] + ' takes one out, clears it all out ' 
-        + [j] + ' lines of code in the file.');
-    } else {
-    console.log([j] + ' lines of code in the file, ' + [j] +
-    ' lines of code; ' + friends[i] + 
-    ' takes one out, clears it all out,no more lines of code in the file.');
+        ' lines of code; ' + friends[i] + ' takes one out, clears it all out, ' 
+        + [j - 1] + ' lines of code in the file.');
+    } 
+    else if(j < 2) {
+    console.log([j] + ' line of code in the file, ' + [j] +
+    ' line of code; ' + friends[i] + 
+    ' takes one out, clears it all out, no more lines of code in the file.');
     }
 }
 }
