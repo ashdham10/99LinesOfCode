f696bf21e421897cb3666a118dda484080fdcaad
ashdham10 <ashleydenham10@gmail.com>
2018-09-11 10:14:58 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+let friends = ['Jordin', 'Felesha', 'Amanda', 'Britt', 'Mike'];
+
+var value = [j];
+
+for (var i = 0; i < friends.length; i++) {
+
+    console.log(friends[i]);
+
+    for (var j = 99; j <= 1; j--) {
+
+        console.log(value[j] [i]);
+
+        console.log(value[j] + 'lines of code in the file' + value[j] + 'lines of code,'
+        + friends[i] + 'takes one out, clears it all out' + value[j] + 
+        'lines of code in the file.');
+    }
+}
